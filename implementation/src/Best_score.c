5f384e3077d4641b29a8f91f55d230c2d36c8e82
chayvakacharla <89779094+chayvakacharla@users.noreply.github.com>
2021-09-07 16:06:06 +0530

    Update Best_score.c

--- implementation/src/Best_score.c
+++ implementation/src/Best_score.c
@@ -1,33 +1,33 @@
 #include<Best_score.h>
 
-int win( int board[9]) {
+int win( int b[9]) {
 
-    unsigned wins[8][3] = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    unsigned w[8][3] = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     int i;
     for(i = 0; i < 8; ++i) {
-        if(board[wins[i][0]] != 0 &&
-           board[wins[i][0]] == board[wins[i][1]] &&
-           board[wins[i][0]] == board[wins[i][2]])
-            return board[wins[i][2]];
+        if(b[w[i][0]] != 0 &&
+           b[w[i][0]] == b[w[i][1]] &&
+           b[w[i][0]] == b[w[i][2]])
+            return b[w[i][2]];
     }
     return 0;
 }
-int minimax(int board[9], int player) {
+int minimax(int b[9], int p) {
     int winner = win(board);
-    if(winner != 0) return winner*player;
+    if(winner != 0) return winner*p;
 
    int move = -1;
     int score = -2;
     int i;
     for(i = 0; i < 9; ++i) {
-        if(board[i] == 0) {
-            board[i] = player;
+        if(b[i] == 0) {
+            b[i] = p;
             int thisScore = -minimax(board, player*-1);
             if(thisScore > score) {
                 score = thisScore;
                 move = i;
             }
-            board[i] = 0;
+            b[i] = 0;
         }
     }
     if(move == -1) return 0;
